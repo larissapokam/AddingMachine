e6ae6ce5d11fb5ace2cea2aa3fbd513f355d77da
pokam <pokamlarissa@gmail.com>
2019-06-11 23:48:32 -0700

    First commitment

---
+++ Project2_AddingMachine/cse360assign2/AddingMachine.java
@@ -0,0 +1,84 @@
+package cse360assign2;
+
+/**
+ * AddingMachine ---- class that acts as a simple calculator
+ * 		while keeping a history of the transactions and returned as a string
+ * @author  Larissa Pokam Epse Takou Kuitche
+ * CSE 360, Summer 2019
+ * Assignment 2
+ * 06 - 11 - 2019
+ */
+
+
+public class AddingMachine {
+
+	/**
+	 * Represents the total for all operation
+	 */
+	
+	private int total;
+	
+	/**
+	 * Class constructor.
+	 * @param no parameter value.
+	 */
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	
+	/**
+	 * getTotal method that allow to obtain the current total.
+	 * @param No parameter value.
+	 * @return an integer representing the current total.
+	 */
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	
+	/**
+	 * The add method that add the given parameter value to the total variable
+	 * @param  value represents the value to be added to the current total.
+	 * @return No parameter value.
+	 */
+	
+	public void add (int value) {
+		
+	}
+	
+	
+	/**
+	 * The subtract method subtracts the parameter from the total variable.
+	 * @param  value represents the value to be subtracted from the total.
+	 * @return No parameter value.
+	 */
+	
+	public void subtract (int value) {
+		
+	}
+	
+	
+	/**
+	 * The toString keep a history of the transactions. 
+	 * @param  No parameter value.
+	 * @return string representing the history of the transactions kept.
+	 */
+	
+	public String toString () {
+		return "";
+	}
+
+
+	/**
+	 * The clear method erased a history of the transactions. 
+	 * @param  No parameter value.
+	 * @return No parameter value.
+	 */
+	
+	public void clear() {
+	
+	}
+}
